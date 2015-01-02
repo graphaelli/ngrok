86c545291bb1ecdff18af7e5b524dbc08c568013
Gil Raphaelli <graphaelli@monetate.com>
2015-01-01 22:21:29 -0500

    reorganize for go getability

---
+++ ngrok.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/graphaelli/ngrok/client"
+)
+
+func main() {
+	client.Main()
+}
---
+++ server/daemon/ngrokd.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/graphaelli/ngrok/server"
+)
+
+func main() {
+	server.Main()
+}
